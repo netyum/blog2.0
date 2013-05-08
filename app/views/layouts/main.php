5f5e9fb7fd96b5108d6ea3c1ff5241fcba0ba9d0
syang <netyum@163.com>
2013-05-08 09:27:39 +0800

    add viewport

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -10,6 +10,7 @@ $this->beginPage();
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo Html::encode($this->title); ?></title>
 	<?php $this->head(); ?>
 </head>
