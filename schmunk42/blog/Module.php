2820a94ca046ad7a87bc7e0458183ed9eeae3d50
Tobias Munk <schmunk@usrbin.de>
2013-06-06 22:22:25 +0200

    autoload refactoring

---
+++ schmunk42/blog/Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tobias
+ * Date: 06.06.13
+ * Time: 22:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace schmunk42\blog;
+
+
+class Module extends \yii\base\Module {
+
+}
